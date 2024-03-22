56922ad8083f7606af4eba472e117b544668e9b2
obkin <kapellwork@gmail.com>
2024-03-22 11:49:04 +0200

    added: two ways DB connection: dev & prod (lib: cross-env) / added: users -> UR

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [],
   controllers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'mac',
-      password: '123456',
-      database: 'nest-test-server-db',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
