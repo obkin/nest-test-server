6fdfa61ef1470681671b60cfcbbda36f8d89194c
obkin <kapellwork@gmail.com>
2024-03-28 00:23:11 +0200

    added: user-create.dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,4 @@
+export class UserCreateDto {
+  readonly email: string;
+  readonly password: string;
+}
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { UserModel } from './user.model';
+import { UserCreateDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersRepository {}
+export class UsersRepository {
+  constructor(
+    @InjectModel(UserModel) private usersRepository: typeof UserModel,
+  ) {}
+
+  createNewUser(dto: UserCreateDto) {
+    try {
+      this.usersRepository.create(dto);
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  getAllUsers() {
+    try {
+      //
+    } catch (e) {
+      //
+    }
+  }
+}
